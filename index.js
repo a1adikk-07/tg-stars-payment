4bd50f429e34bb648f7e0ba0a3a6774f2b0f9589
a1adikk-07 <kolomoecvladislav73@gmail.com>
2026-02-22 23:56:57 +0100

    welcoming messages

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ const token = process.env.BOT_TOKEN;
 const bot = new tgBotApi(token, { polling: true });
 
 bot.on('message', msg => {
+    const chatId = msg.chat.id;
+    const msgText = msg.text;
+
+    if (msgText === '/start') {
+        bot.sendSticker(chatId, 'https://cdn.cdnstep.com/veHqCAZALku5D0hmtCUf/4.thumb128.webp');
+        bot.sendMessage(chatId, `Hello, ${msg.from.first_name}!\n\Welcome to the test Telegram bot with a payment system using Telegram Stars.`);
+    }
+
     console.log(msg);
 })
